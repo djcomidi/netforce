3e968a404802392b0af9c2f2cc5b580858cf7d0d
Stijn Vanden Eynde <djcomidi@gmail.com>
2015-01-10 23:54:14 +0100

    javascript: level102: adding solution

---
+++ javascript/level102/solution.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+numletter = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+sol = ""
+sol += numletter[11]
+sol += numletter[18]
+sol += numletter[23]
+sol += numletter[16]
+sol += numletter[24]
+sol += numletter[1:4]
+print sol
+
