1b826e1fcbd78183782eaf2929e4825f60901282
Stijn Vanden Eynde <djcomidi@gmail.com>
2015-07-07 15:15:53 +0200

    programming: level607: adding solution

---
+++ programming/level607/solution.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+num = 13155187
+d = 13417
+divs = []
+while len(divs) < 5001:
+    q, r = divmod(num,d)
+    divs.append(q)
+    num = 10*r
+sol = "".join( str(i) for i in divs[-6:] )
+print(sol)
