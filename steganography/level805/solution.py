97beef62e3190524877aa73a9abb78fb59fdae24
Stijn Vanden Eynde <djcomidi@gmail.com>
2015-07-11 21:47:29 +0200

    steganography: level805: adding solution

---
+++ steganography/level805/solution.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from urllib import request
+import re
+
+url = "https://net-force.nl/challenge/level805/monster.jpg"
+with request.urlopen(url) as img:
+    bytes = img.read()
+longest = b''
+for m in re.findall(b'[01]+', bytes):
+    if len(m) > len(longest):
+        longest = m
+data = longest.decode()
+print(data)
+msg = "".join( chr(int(data[i:i+8],2)) for i in range(0,len(data),8) )
+print(msg)
